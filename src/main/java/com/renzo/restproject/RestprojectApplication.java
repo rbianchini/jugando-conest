03dc3a406eba4271dcfb25f26b463519ee18057c
Renzo Bianchini <renzobianchini@gmail.com>
2021-01-21 18:49:52 -0300

    Basic Rest Completed

--- src/main/java/com/renzo/restproject/RestprojectApplication.java
+++ src/main/java/com/renzo/restproject/RestprojectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestprojectApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RestprojectApplication.class, args);
+  }
 
 }
